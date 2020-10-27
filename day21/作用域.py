39e3ade7429c3ff3f298e979b56344abd6482518
cxylishang <17853481576@163.com>
2020-10-27 16:12:32 +0800

    python_file

---
+++ day21/作用域.py
@@ -0,0 +1,22 @@
+#-*- coding:utf-8 -*-
+# Auther: LiShang
+
+# def func():
+#     if 1==1:
+#         name = 'lishang'
+#     print(name)
+#
+# func()
+#
+#
+#
+#
+# def f():
+#     if 2==2:
+#         name = 'zhaoxiaomeng'
+#     print(name)
+#
+# f()
+# print(name)
+a = 100
+print("test1-------修改前：%d" %a)
